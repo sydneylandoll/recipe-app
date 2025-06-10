e65b25fa26d74d0eba9b019210986ea0d52a9574
Sydney Landoll <slandoll384@gmail.com>
2025-06-10 17:00:06 -0400

    Created app.py, which lists .md files

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+
+recipe_folder = "recipes"
+
+files = os.listdir(recipe_folder)
+
+md_files = [f for f in files if f.endswith(".md")]
+
+for i, file_name in enumerate(md_files, 1):
+    print(f"{i}. {file_name}")
